fdd8d0f0ea8e34bafbf8656cd31d07829de9df26
CashUnionPay <98192339+CashUnionPay@users.noreply.github.com>
2022-02-03 08:24:27 -0600

    Update and rename whitelist1.js to whitelist1forTestNet.js

---
+++ scripts/whitelist1forTestNet.js
@@ -0,0 +1,6 @@
+const whitelist = [
+    ["0xD1ac12C7D8b1001fba21a9a86ced0C07bbb517aE", 5414062.888, false],
+    ["0xf02d1dCC9c7B11575Fa353F86aA9fa2b7C4751e4", 1000.000, false],
+]
+
+module.exports = { whitelist }
